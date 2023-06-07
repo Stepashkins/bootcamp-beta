5e8f3fc70a3c83e34ac84d2aff69684849b3b3bd
Stepan Palkov <step.palkov@gmail.com>
2023-06-07 13:09:26 +0300

    add methods without realization

---
+++ src/main/java/ru/faang/school/task_2/DataCenterService.java
@@ -0,0 +1,20 @@
+package ru.faang.school.task_2;
+
+public class DataCenterService {
+    public void addServers(Server server){
+
+    }
+    public void removeServers(Server server){
+
+    }
+    public  void getTotalEnergyConsumption(){
+
+    }
+    public void allocateResources(ResourceRequest request){
+
+    }
+    public void releaseResources(ResourceRequest request){
+
+    }
+
+}
---
+++ src/main/java/ru/faang/school/task_2/Server.java
@@ -0,0 +1,8 @@
+package ru.faang.school.task_2;
+
+public class Server {
+    private double load;
+    private double maxLoad;
+    private double energyConsumption;
+
+}
